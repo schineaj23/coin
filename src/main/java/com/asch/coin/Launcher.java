26a85edd67fc85dfaac3f3a034602491134b070a
Andrew Schineller <schineaj23@pwcs-edu.org>
2023-05-11 11:03:24 -0400

    Add stuff for JAR

---
+++ src/main/java/com/asch/coin/Launcher.java
@@ -0,0 +1,9 @@
+package com.asch.coin;
+
+import java.security.NoSuchAlgorithmException;
+
+public class Launcher {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InterruptedException {
+        Main.main(args);
+    }
+}
